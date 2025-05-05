1a739607975de343a582e82a4a8a6b59c57038db
Ross Tungol <rossjstungol@gmail.com>
2025-05-05 16:47:43 +0800

    Implements sign up function

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,16 @@
 import { config } from "dotenv";
+import { z } from "zod";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI } = process.env;
+const envSchema = z.object({
+  PORT: z.string().default("5500"),
+  NODE_ENV: z.string().default("development"),
+  DB_URI: z.string().min(1, "DB_URI is required"),
+  JWT_SECRET: z.string().min(1, "JWT_SECRET is required"),
+  JWT_EXPIRES_IN: z.string().default("1d"),
+});
+
+const env = envSchema.parse(process.env);
+
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = env;
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,71 @@
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.ts";
+import { CustomError } from "../types/index.ts";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.ts";
+
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if a user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error: CustomError = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const signOut = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
