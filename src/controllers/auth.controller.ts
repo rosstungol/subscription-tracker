011319c9ff9e489b9b524f71a5a98479f337777e
Ross Tungol <rossjstungol@gmail.com>
2025-05-06 16:05:06 +0800

    Implements sign in function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -62,7 +62,42 @@ export const signIn = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error: CustomError = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error: CustomError = new Error("Invalid password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const signOut = async (
   req: Request,
