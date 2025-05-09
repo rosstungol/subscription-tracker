d4f574cb2bb95cd971b9978eb97ef790e5129643
Ross Tungol <rossjstungol@gmail.com>
2025-05-09 17:30:59 +0800

    Arcjet config setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,8 +9,18 @@ const envSchema = z.object({
   DB_URI: z.string().min(1, "DB_URI is required"),
   JWT_SECRET: z.string().min(1, "JWT_SECRET is required"),
   JWT_EXPIRES_IN: z.string().default("1d"),
+  ARCJET_KEY: z.string().min(1, "ARCJET_KEY is required"),
+  ARCJET_ENV: z.string().default("development"),
 });
 
 const env = envSchema.parse(process.env);
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_KEY,
+  ARCJET_ENV,
+} = env;
